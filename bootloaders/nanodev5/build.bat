54c59bba289038f7185bca28570ade1802565c23
Andrew Lindsay <andrew@thiseldo.co.uk>
2011-08-14 19:07:06 +0100

    Removed suprlus file

--- bootloaders/nanodev5/build.bat
+++
@@ -1,4 +0,0 @@
-make 
-avrdude -c usbtiny -p m328p -U flash:w:optiboot_nanodev5.hex -U lfuse:w:0xff:m -U hfuse:w:0xda:m -U efuse:w:0x05:m 
-avrdude -c usbtiny -p m328p -U lfuse:w:0xff:m -U hfuse:w:0xda:m -U efuse:w:0x05:m 
-
